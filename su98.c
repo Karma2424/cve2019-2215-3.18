1cbbfda9353a7487df8af8646b53d62a7b3ac5ca
arpruss <arpruss@gmail.com>
2019-10-25 12:08:19 -0500

    threads

--- su98.c
+++ su98.c
@@ -21,7 +21,7 @@
 #define OFFSET__cred__cap_bset (OFFSET__cred__cap_permitted+0x010)
 
 #define BINDER_SET_MAX_THREADS 0x40046205ul
-#define MAX_THREADS 2
+#define MAX_THREADS 3
 
 #define RETRIES 3
 
