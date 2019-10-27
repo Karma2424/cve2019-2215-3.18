dabee1fc06ff3e5fd80f6e6d6f0197a70b33ba0d
arpruss <arpruss@gmail.com>
2019-10-26 22:07:56 -0500

    foo

--- app/src/main/assets/install.sh
+++ app/src/main/assets/install.sh
@@ -1,3 +1,4 @@
 mount -t tmpfs tmpfs /sbin && cp /data/data/mobi.omegacentauri.su98/su98 /sbin && cd /sbin && ln -s su98 su && chmod 755 su98 \
-    && ln -s /data/local/tmp/su98-whitelist.txt . && ln -s /data/local/tmp/su98-denied.txt .
-    
+    && ln -s /data/local/tmp/su98-whitelist.txt . && ln -s /data/local/tmp/su98-denied.txt . \
+    && cd /data/local/tmp && touch su98-denied.txt && chown shell.shell su98-denied.txt \
+    && chmod 644 /data/local/tmp/su98-denied.txt 
