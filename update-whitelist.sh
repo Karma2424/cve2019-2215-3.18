a5053236ab7d59d66f0761bd2e4cf13e5ae44fd9
arpruss <arpruss@gmail.com>
2019-10-27 00:28:48 -0500

    foo

--- update-whitelist.sh
+++ update-whitelist.sh
@@ -1,2 +1,3 @@
-cat /data/local/tmp/su98-denied.txt /data/local/tmp/su98-whitelist.txt | sort | uniq > /data/local/tmp/su98-whitelist.txt
-cat /dev/null > /data/local/tmp/su98-denied.txt
+cd /data/local/tmp/su98-denied
+tail -1 su98-denied.txt | cat - su98-whitelist.txt | sort | uniq > /data/local/tmp/su98-whitelist.txt
+sed -i '$d' su98-denied.txt
